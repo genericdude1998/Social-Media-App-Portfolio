3a96e46e93a940216d3e6544b770f08010242fe5
vsenzo <vsenzo@hello.com>
2021-11-03 14:01:59 +0000

    added pictures background and webpack config

--- src/view/LoginForm.jsx
+++ src/view/LoginForm.jsx
@@ -6,15 +6,13 @@ const LoginForm = () => {
     const [password, setPassword] = React.useState('');
 
     return (
-        <div className={styles.baseDiv}>
-            <form className={styles.baseForm}>
-                <label className={styles.baseLabel}>Username:</label>
-                <input type='text' className={styles.baseInput}/>
-                <label className={styles.baseLabel}>Password:</label>
-                <input type='text'/>
-                <button type='submit' className={styles.baseButton}>Login</button>
-            </form>
-        </div>
+        <form className={styles.baseForm}>
+            <label className={styles.baseLabel}>Username:</label>
+            <input type='text' className={styles.baseInput}/>
+            <label className={styles.baseLabel}>Password:</label>
+            <input type='password'className={styles.baseInput}/>
+            <button type='submit' className={styles.baseButton}>Login</button>
+        </form>
     );
 }
 
