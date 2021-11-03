5ecdd18b9d4c71b6b7373d81b21e3a917660d869
vsenzo <vsenzo@hello.com>
2021-11-03 13:44:23 +0000

    added css skeleton of login form

--- src/view/LoginForm.jsx
+++ src/view/LoginForm.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import styles from './LoginForm.css'
 
 const LoginForm = () => {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
 
     return (
-        <div>
-            <form>
-                <label>Username:</label>
+        <div className={styles.baseDiv}>
+            <form className={styles.baseForm}>
+                <label className={styles.baseLabel}>Username:</label>
+                <input type='text' className={styles.baseInput}/>
+                <label className={styles.baseLabel}>Password:</label>
                 <input type='text'/>
-                <label>Username:</label>
-                <input type='text'/>
-                <button type='submit'>Login</button>
+                <button type='submit' className={styles.baseButton}>Login</button>
             </form>
         </div>
     );
