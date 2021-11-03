512e6170b695fa7b39da7ad3c8fc4896f01a1278
vsenzo <vsenzo@hello.com>
2021-11-03 11:42:12 +0000

    added post route to devServer login

--- server/server.js
+++ server/server.js
@@ -1,16 +1,32 @@
 const credentials = require('./credentials.js');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const devServer = (devServer) => {
     const app = devServer.app;
     
     app.use(require('cookie-parser')(credentials.cookieSecret));
+    app.use(express.json());
 
-    app.get('/', (req,res) => {
-        const token = generateJWT('sample_username');
-        res.cookie('test_JWT_Token', token);
-        res.send('sent a JWT token via cookie');
+    app.post('/login', (req,res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if(authenticateUser(username, password)){
+            const token = generateJWT(username);
+            res.cookie('Mock_JWT_token', token);
+            res.send('sent a JWT token via cookie');
+        }
+        else{
+            res.send('Failed Authentication');
+        }
     })
+}
+
+function authenticateUser(username,password){
+    if(username && password){
+        return username === 'Adam' && password === 1234 ? true:false
+    }
 }
 
 function generateJWT(username){
