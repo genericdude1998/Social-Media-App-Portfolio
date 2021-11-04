1b361338d5feb66a13bea06c7af530b9505c1f42
vsenzo <vsenzo@hello.com>
2021-11-04 11:57:18 +0000

    added redux thunk creator and middlewre

--- src/model/actionCreators/actionCreators.js
+++ src/model/actionCreators/actionCreators.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { actionTypes } from '../actionTypes/actionTypes';
 
 export function doSetUsername(username){
@@ -6,4 +7,24 @@ export function doSetUsername(username){
 
 export function doSetPassword(password){
     return {type: actionTypes.SET_PASSWORD, password:password};
+}
+
+export function doLoginRequestSuccess(token){
+    return {type: actionTypes.LOGIN_REQUEST_SUCCESS, token: token}
+}
+export function doLoginRequestFailure(error){
+    return {type: actionTypes.LOGIN_REQUEST_SUCCESS, error: error}
+}
+
+export function doSendUsernameAndPasswordThunk(username, password){
+    return function(dispatch){
+        dispatch(actionTypes.SEND_LOGIN_REQUEST);
+        axios.post('/login', {
+            username:username,
+            password:password,
+        }).then((res) => {
+            const token = res.data;
+            dispatch(doLoginRequestSuccess(token));
+        }).catch(error => dispatch(doLoginRequestFailure(error)));
+    }
 }
--- src/model/store/store.js
+++ src/model/store/store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware } from 'redux';
 import { createStore } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { loginReducer } from '../reducers/reducers';
 
-const middleWare = applyMiddleware(logger);
+const middleWare = applyMiddleware(logger, thunk);
 
 export const store = createStore(loginReducer, middleWare);
