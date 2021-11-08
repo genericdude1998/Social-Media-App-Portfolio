4529c3d2819415a6d6f4c6c173c0d49e8b30ffeb
vsenzo <vsenzo@hello.com>
2021-11-08 13:45:26 +0000

    added login route

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import ConnectedLogin from './view/Login/Login';
 import {store} from './model/store/store';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedLogin />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ConnectedLogin />}/>
+            </Routes>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
