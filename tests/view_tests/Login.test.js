2eb18f95a48daca209c0d7880fe1b6afd8048815
vsenzo <vsenzo@hello.com>
2021-11-09 10:48:42 +0000

    clean-up

--- tests/view_tests/Login.test.js
+++ tests/view_tests/Login.test.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from 'react-redux';
-import {store} from '../../src/model/store/store';
 import ConnectedLogin from '../../src/view/Login/Login';
 import LoginForm from '../../src/view/Login/LoginForm';
 import {mockOnChangeUsername, mockOnChangePassword, mockOnSubmitLogin} from '../mockValues';
-
-
-// jest.mock('react-redux', () => {
-//     return {
-//         connect: (mapStateToProps, mapDispatchToProps) => (ReactComponent) => ({
-//             mapStateToProps,
-//             mapDispatchToProps,
-//             ReactComponent,
-//         }),
-//         Provider: ({ children }) => children,
-//     }
-// })
 
 let loginWrapper;
 beforeEach(() => {
