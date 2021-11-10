73af799a25d243b8a6a06beb33091f788290bf49
vsenzo <vsenzo@hello.com>
2021-11-10 14:14:48 +0000

    added css in user component

--- src/view/UsersInfo/User.jsx
+++ src/view/UsersInfo/User.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import {mapUserDispatchToProps, mapUserStateToProps} from '../mappers/mappers';
+import styles from './User.css';
 
 const User = ({onFetchUser, user}) => {
     const {id} = useParams();
@@ -9,15 +10,15 @@ const User = ({onFetchUser, user}) => {
         onFetchUser(id);
     }, [id]);
     return (
-        <div>
-            {user ?
-                <>
+        <div className={styles.baseUser}>
+            {user ? 
+                <div className={styles.baseUserContent}>
                     <h1>{user.name}</h1>
                     <h3>City: {user.city}</h3>
                     <h3>Joined: {user.joined}</h3>
                     <h3>Description:</h3>
                     <p>{user.desc}</p>
-                </>
+                </div>
                 :
                 <h1>Loading</h1>
             }
