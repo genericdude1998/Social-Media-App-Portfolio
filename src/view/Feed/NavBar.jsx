468564b72e491452cf9477886951c342d9c06fd7
vsenzo <vsenzo@hello.com>
2021-11-10 14:29:58 +0000

    added css to the navbar

--- src/view/Feed/NavBar.jsx
+++ src/view/Feed/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import UserLink from './UserLink';
+import styles from './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <Link to='/feed'>Feed</Link>
-            <UserLink id={1} name={'Me'}></UserLink>
+        <div className={styles.baseNavBar}>
+            <div className={styles.baseNavBarContent}>
+                <Link to='/feed' className={styles.baseNavBarLink}>Feed</Link>
+                <UserLink id={1} name={'Me'} className={styles.baseNavBarLink}></UserLink>
+            </div>
         </div>
     );
 }
--- src/view/Feed/UserLink.jsx
+++ src/view/Feed/UserLink.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const UserLink = ({id, name}) => {
+const UserLink = ({id, name, className}) => {
     return (
-        <Link to={`/userInfo/${id}`}>{name}</Link>
+        <Link to={`/userInfo/${id}`} className={className}>{name}</Link>
     );
 }
 
