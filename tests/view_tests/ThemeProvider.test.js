9bdec0eebf1cb6c5ad48640ea158805d7a29776a
vsenzo <vsenzo@hello.com>
2021-11-15 15:05:13 +0000

    added test for theme provider

--- tests/view_tests/ThemeProvider.test.js
+++ tests/view_tests/ThemeProvider.test.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { shallow } from 'enzyme';
-import ConnectedThemeProvider from '../../src/view/theme/ThemeProvider';
+import ConnectedThemeProvider, { ThemeProvider } from '../../src/view/theme/ThemeProvider';
 import { mockTheme } from '../mockValues';
-
-
 
 let wrapper;
 const mockOnToggleTheme = jest.fn();
@@ -11,13 +9,18 @@ const mockOnToggleTheme = jest.fn();
 wrapper = shallow(
     <ConnectedThemeProvider.WrappedComponent
         theme={mockTheme}
-        onToggleTheme={mockOnToggleTheme}
+        onToggleTheme={mockOnToggleTheme}  
     />
 );
+
+console.log(wrapper.debug());
 
 describe('ThemeProvider', () => {
     it('should pass down theme variable to ThemeContextProvider and onToggleTheme', () => {
         expect(wrapper.prop('value').theme).toBe(mockTheme);
         expect(wrapper.prop('value').onToggleTheme).toBe(mockOnToggleTheme);
+    });
+    it('should matchSnapshot', () => {
+        expect(wrapper).toMatchSnapshot();
     });
 });
